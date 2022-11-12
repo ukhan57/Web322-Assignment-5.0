ac85e52f04ef16f5fdc8dd6382bcd33423f5de32
ukhan57 <ukhan57@myseneca.ca>
2022-11-11 21:55:13 -0500

    Commit

--- server.js
+++ server.js
@@ -6,6 +6,46 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const handlebars = require("express-handlebars");
 const mongoose = require("mongoose");
+const { read } = require("fs");
+const schema = mongoose.Schema;
+
+// MONGO DB
+// puu7YLdL10b6bccG - Password
+
+const registration = mongoose.createConnection("mongodb+srv://Admin:puu7YLdL10b6bccG@cluster0.qkcbfzy.mongodb.net/?retryWrites=true&w=majority");
+const blog = mongoose.createConnection("mongodb+srv://Admin:puu7YLdL10b6bccG@cluster0.qkcbfzy.mongodb.net/?retryWrites=true&w=majority");
+const article = mongoose.createConnection("mongodb+srv://Admin:puu7YLdL10b6bccG@cluster0.qkcbfzy.mongodb.net/?retryWrites=true&w=majority");
+
+const registrationSchema = new schema({
+    "fName": String,
+    "lName": String,
+    "email": String,
+    "username": {
+        "type": String,
+        "unique": true
+    },
+    "Address": String,
+    "city": String,
+    "postalCode": String,
+    "country": String,
+    "password": {
+        "type": String,
+        "unique": true
+    }
+});
+
+const blogSchema = new schema({
+    "title": String,
+    "content": String
+});
+
+const articleSchema = new schema({
+    "read": String
+});
+
+const userInfo = registration.model("registration", registrationSchema);
+const blogContent = blog.model("blogDB", blogSchema);
+const articleContent = article.model("articleDB", articleSchema);
 
 const HTTP_PORT = process.env.PORT || 8080;
 
@@ -17,7 +57,6 @@ function onHttpStart() {
 // Register handlebars as the rendering engine for views
 app.engine(".hbs", handlebars.engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
-
 app.use(bodyParser.urlencoded({ extended: true}));
 
 //Router function for '/' page
@@ -52,72 +91,73 @@ app.post("/login", function(req,res){
 
 });
 
-//Router function for 'registration' page
-app.get("/registration", function(req,res){
-    res.sendFile(path.join(__dirname, "/registration.html"));
-});
+// //Router function for 'registration' page
+// app.get("/registration", function(req,res){
+//     res.sendFile(path.join(__dirname, "/registration.html"));
+// });
 
-app.post("registration", function(req,res){
+// app.post("registration", function(req,res){
 
-    var userdata = {
-        fname: req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
-        phonenumber: req.body.phonenumber,
-        city: req.body.city,
-        phonetest: /^\d{10}$/.test(req.body.phonenumber),
-        Address: req.body.Address1,
-        postalcode: req.body.postalcode,
-        postaltest: /^[AaBbCcEeGgHiJjKkLlMmNnPpRrSsTtVvXxYy]\d[A-Za-z] \d[A-Za-z]\d$/.test(req.body.postalcode),
-        country: req.body.country,
-        password: req.body.password,
-        passwordtest: /^[0-9a-zA-Z]{6,12}$/.test(req.body.password),
-        confirmpassword: req.body.confirmpassword,
-    }
+//     var userdata = {
+//         fName: req.body.fname,
+//         lName: req.body.lname,
+//         email: req.body.email,
+//         phonenumber: req.body.phonenumber,
+//         city: req.body.city,
+//         phonetest: /^\d{10}$/.test(req.body.phonenumber),
+//         Address: req.body.Address,
+//         postalCode: req.body.postalcode,
+//         postaltest: /^[AaBbCcEeGgHiJjKkLlMmNnPpRrSsTtVvXxYy]\d[A-Za-z] \d[A-Za-z]\d$/.test(req.body.postalcode),
+//         country: req.body.country,
+//         password: req.body.password,
+//         passwordtest: /^[0-9a-zA-Z]{6,12}$/.test(req.body.password),
+//         confirmpassword: req.body.confirmpassword,
+//     }
 
-    checkpass = function() {
-        if (userdata.password == userdata.confirmpassword) {
-            return true;
-        }
-        return false;
-    }
+//     var checkpass = function() {
+//         if (userdata.password == userdata.confirmpassword) {
+//             return true;
+//         }
+//         return false;
+//     }
 
-    userdata.checkpassword = checkpass;
+//     userdata.checkpassword = checkpass;
 
-    if (userdata.fname == "" ||
-        userdata.lname == "" ||
-        userdata.email == "" ||
-        userdata.phonenumber == "" ||
-        userdata.Address1 == "" ||
-        userdata.city == "" ||
-        userdata.postalcode == "" ||
-        userdata.country == "" ||
-        userdata.password == "" ||
-        userdata.confirmpassword == "") {
+//     if (userdata.fname == "" ||
+//         userdata.lname == "" ||
+//         userdata.email == "" ||
+//         userdata.phonenumber == "" ||
+//         userdata.Address == "" ||
+//         userdata.city == "" ||
+//         userdata.postalcode == "" ||
+//         userdata.country == "" ||
+//         userdata.password == "" ||
+//         userdata.confirmpassword == "") {
 
-        res.render("registration", { data: userdata, layout: false });
-        return;
-    }
+//         res.render("registration", { data: userdata, layout: false });
+//         return;
+//     }
 
-    if (!userdata.phonetest) {
-        res.render("registration", { data: userdata, layout: false });
-        return;
-    }
-    if (!userdata.postaltest) {
-        res.render("registration", { data: userdata, layout: false });
-        return;
-    }
-    if (!userdata.passwordtest) {
-        res.render("registration", { data: userdata, layout: false });
-        return;
-    }
-    if (!userdata.checkpassword) {
-        res.render("registration", { data: userdata, layout: false });
-        return;
-    }
+//     if (!userdata.phonetest) {
+//         res.render("registration", { data: userdata, layout: false });
+//         return;
+//     }
+//     if (!userdata.postaltest) {
+//         res.render("registration", { data: userdata, layout: false });
+//         return;
+//     }
+//     if (!userdata.passwordtest) {
+//         res.render("registration", { data: userdata, layout: false });
+//         return;
+//     }
+//     if (!userdata.checkpassword) {
+//         res.render("registration", { data: userdata, layout: false });
+//         return;
+//     }
 
-    res.render("dashboard", { layout: false });
-});
+    
+//     res.render("dashboard", { layout: false });
+// });
 
 // start the server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
