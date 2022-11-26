976ac247840d17d9bd51bdec5eefb179d4b99792
ukhan57 <ukhan57@myseneca.ca>
2022-11-25 23:54:36 -0500

    Commit

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const handlebars = require("express-handlebars");
 const mongoose = require("mongoose");
 const { read } = require("fs");
+const { log } = require("util");
 const schema = mongoose.Schema;
 
 // MONGO DB
@@ -35,10 +36,13 @@ const registrationSchema = new schema({
 
 const blogSchema = new schema({
     "title": String,
-    "date": String
+    "content": String,
+    "date": String,
+    "image": String
 });
 
 const articleSchema = new schema({
+    "title":String,
     "read": String
 });
 
@@ -64,13 +68,59 @@ app.get("/", function(req,res){
 });
 
 app.get("/blog", function(req,res){
-    res.render("blog", {layout:false});
+    blogContent.find().exec().then((data) => {
+        let log = new Array;
+        data.forEach(element => {
+            log.push({
+                title: element.title,
+                date: element.date,
+                content: element.content,
+                image: element.image
+            });
+        });
+        res.render("blog", {title: log, layout:false});
+    });
 });
 
-app.get("/read_more", function(req,res){
-    res.render("read_more", {layout:false});
+app.get("/admin", function(req,res){
+    res.render("admin", {layout:false});
 });
 
+app.post("/admin", function(req,res){
+    let articelData = new blogContent({
+        title: req.body.title,
+        content: req.body.content,
+        date: req.body.data,
+        image: req.body.img
+    }).save((e, data) => {
+        if(e) {
+            console.log(e);
+        } else {
+            console.log(data);
+        }
+    });
+    res.redirect("/");
+});
+
+app.post("/read_more", function(req,res){
+    blogContent.findOne({title: req.body.title}).exec().then((data) => {
+        res.render("read_more", {image: data.image, read: data.content, title: data.title, date: data.date, layout:false});
+    });
+});
+
+app.post("/update", (req, res) => {
+    blogContent.updateOne({
+        _id: req.body.ids
+    }, {
+        $set: {
+            title: req.body.title,
+            date: req.body.dat,
+            content: req.body.content,
+            image: req.body.img
+        }
+    }).exec();
+    res.redirect("/");
+});
 
 app.get("/login", function(req,res){
     res.sendFile(path.join(__dirname, "/login.html"));
@@ -94,14 +144,18 @@ app.post("/login", function(req,res){
         res.render("login", { data: userdata, layout: false });
         return;
     }
-    else {
+    else 
+    {
         userInfo.findOne({username: userdata.user, password: userdata.pass}, ["fName", "lName", "username"]).exec().then((data) =>{
-            if(data = "63707446d3e718b7d0f4a325"){
+            if(data = "63815d4ca97e8e85fb46dc8e"){
                 res.render("Admin_Dashboard", {fName: data.fName, lName: data.lName, username: data.username, layout: false});
+                return;
             }
             else {
                 res.render("User_Dashboard", {data: userdata, layout:false});
+                return;
             }
+             
         });
     }
     
@@ -186,12 +240,13 @@ app.post("/registration", function(req,res){
     res.render("dashboard", {layout:false});
 });
 
-app.get("", function(req,res){
-    res.status(404).send("Page not found!");
+//Router function for page not found
+app.get("/error", function(req,res){
+    res.sendFile(path.join(__dirname, "/error.html"));
 });
 
 //Router to using images in .html giles
-app.use(express.static("static"));
+app.use(express.static("img"));
 
 // start the server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
